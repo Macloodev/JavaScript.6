f238d89933af7e2c8afae8d17bc1c2eef955ab4c
Macloodev <crosby_2@mail.ru>
2023-07-03 15:03:34 +0300

    first

---
+++ task2/script.js
@@ -0,0 +1,19 @@
+const consolelog =  document.querySelector('#consoleLog');
+
+consolelog.addEventListener('click', () => {
+    alert('Служит для вывода информации в консоль')
+})
+
+const alerta =  document.querySelector('#alerta');
+
+alerta.addEventListener('click', () => {
+    alert('Пример использования команды alert')
+})
+
+const prompta =  document.querySelector('#prompta');
+
+prompta.addEventListener('click', () => {
+    alert('Пример использования команды promt')
+})
+
+
---
+++ task4/script.js
@@ -0,0 +1,1 @@
+const enterTx = document.querySelector('#enterTx');
